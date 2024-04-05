e592bdac0c8a5e896d0732c7965128576c9bd489
Sordid-Slug <126084827+Sordid-Slug@users.noreply.github.com>
2024-04-05 22:44:53 +0300

    Update main.c
    may be done

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int count_bits_of_value(int value, int bit) {
 //     dblLinkedList *list = param_args->list;
 //     int i = 0;
 //     while (list->tail != NULL && list->size > 0) {
-//         printf("единицы [%d] %ld\n", i, list->size);
+//         perror("единицы [%d] %ld\n", i, list->size);
 //         int *value = (int *)list->tail->value;
 //         return_values->count += count_bits(*value, param_args->bit);
 //         return_values->processed += 1;
@@ -50,13 +50,20 @@ int count_bits_of_value(int value, int bit) {
 // }
 
 void *bits_count(void *params) {
-    pthread_mutex_lock(&mutex);
     Params *param_args = (Params *)params;
     Returns *return_values = (Returns *)calloc(1, sizeof(Returns));
+    if (return_values == NULL) {
+        perror("Память не выделилась корректно\n");
+    }
     dblLinkedList *list = param_args->list;
     
     int i = 0;
-    while (param_args->start != NULL && list->size > 0) {
+    while (1) {
+        pthread_mutex_lock(&mutex);
+        if (list->head == 0x0 || list->tail == 0x0) {   // Я не имею ни малейшего понятия, почему, но
+            pthread_mutex_unlock(&mutex);               // условие while (list->head != 0x0 && list->tail != 0x0) не всегда работает,
+            break;                                      // даже когда list->head && list->tail
+        }
         if (param_args->bit == 0) {
             param_args->start = list->head;
         } else {
@@ -68,18 +75,18 @@ void *bits_count(void *params) {
 
         param_args->deleteNodeFunc(list);
         i++;
+        pthread_mutex_unlock(&mutex);
     }
-    pthread_mutex_unlock(&mutex);
     pthread_exit((void *)return_values);
 }
 
 int main(int argc, char **argv) {
     int size;
     if (argc > 5) {
-		printf("Может быть один аргумент - размер списка\n");
+		perror("Может быть один аргумент - размер списка\n");
         exit(1);
     } else if ((size = atoi(argv[1])) == 0 && (*argv)[1] != '0') {
-        printf("Введите только одно число\n");
+        perror("Введите только одно число\n");
         exit(2);
     }
     dblLinkedList* list = createList();
@@ -90,27 +97,16 @@ int main(int argc, char **argv) {
 	}
     pthread_t pid;
     pthread_t pid2;
-    // Делим список на две половины
-    dblLinkedList* listForZeroes = createList();
-    dblLinkedList* listForOnes = createList();
 
-    listForZeroes->head = list->head;
-    listForZeroes->tail = getNode(list, list->size / 2);
-    listForZeroes->size = list->size / 2;
-
-    listForOnes->head = getNode(list, list->size / 2 + 1);
-    listForOnes->tail = list->tail;
-    listForOnes->size = list->size % 2 > 0 ? list->size / 2 + 1 : list->size / 2;
-
-    Params paramsForZeroes = {listForZeroes, 0, popFront, listForZeroes->head};
-    Params paramsForOnes = {listForOnes, 1, popBack, listForOnes->tail};
+    Params paramsForZeroes = {list, 0, popFront, list->head};
+    Params paramsForOnes = {list, 1, popBack, list->tail};
 
     if (pthread_create(&pid, NULL, bits_count, &paramsForZeroes) != 0) {
-        printf("Thread didn't create correctly");
+        perror("Поток не создался корректно");
         exit(5);
     }
     if (pthread_create(&pid2, NULL, bits_count, &paramsForOnes) != 0) {
-        printf("Thread didn't create correctly");
+        perror("Поток не создался корректно");
         exit(6);
     }
     void *res_first_thread;
@@ -121,11 +117,10 @@ int main(int argc, char **argv) {
     Returns* ones = (Returns *)res_second_thread;
 
     printf("\nZeroes:\ncount: %d\nprocessed: %d\n\nOnes:\ncount: %d\nprocessed: %d\n", zeroes->count, zeroes->processed, ones->count, ones->processed);
+    // printf("\nZeroes:\ncount: %d\nprocessed: %d\n", zeroes->count, zeroes->processed);
     free(res_first_thread);
     free(res_second_thread);
 
     free(list);
-    free(listForOnes);
-    free(listForZeroes);
     return 0;
 }
