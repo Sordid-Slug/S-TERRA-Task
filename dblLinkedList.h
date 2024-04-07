a0de9016c7e6918c52b4f6e3fc58d3f6a902b4a5
Sordid-Slug <126084827+Sordid-Slug@users.noreply.github.com>
2024-04-07 12:41:30 +0300

    Update dblLinkedList.h

--- dblLinkedList.h
+++ dblLinkedList.h
@@ -10,16 +10,13 @@ typedef struct _Node {
 typedef struct _dblLinkedList {
 	Node* head;
 	Node* tail;
-	size_t size;
 } dblLinkedList;
 
 dblLinkedList *createList();
 void deleteList(dblLinkedList** list);
 void pushback(dblLinkedList** list, void* value, size_t value_size);
-Node* getNode(dblLinkedList* list, size_t index);
-void delNode(dblLinkedList* list, size_t index);
-void popBack(dblLinkedList* list);
-void popFront(dblLinkedList* list);
+void* popBack(dblLinkedList* list);
+void* popFront(dblLinkedList* list);
 
 //	Чтобы работало для разных типов данных, надо описать функцию печати для этих типов, как printInt 
 
