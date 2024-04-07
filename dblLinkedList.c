379c8e021d193e36a2d9a01a8f1d626c9ace40fd
Sordid-Slug <126084827+Sordid-Slug@users.noreply.github.com>
2024-04-07 12:41:07 +0300

    done

--- dblLinkedList.c
+++ dblLinkedList.c
@@ -7,10 +7,9 @@
 dblLinkedList* createList() {
 	dblLinkedList* tmp = (dblLinkedList*)calloc(1, sizeof(dblLinkedList));
 	if (!tmp) {
-		printf("Память не выделилась корректно\n");
+		perror("Память не выделилась корректно\n");
 		exit(1);
 	}
-	tmp->size = 0;
 	tmp->head = tmp->tail = NULL;
 
 	return tmp;
@@ -32,13 +31,13 @@ void deleteList(dblLinkedList** list) {
 void pushback(dblLinkedList** list, void* value, size_t value_size) {
 	Node* tmp = (Node*)calloc(1, sizeof(Node));
 	if (tmp == NULL) {
-		printf("Память не выделилась корректно\n");
+		perror("Память не выделилась корректно\n");
 		exit(2);
 	}
 
 	tmp->value = calloc(1, value_size);
 	if (tmp->value == NULL) {
-		printf("Память не выделилась корректно\n");
+		perror("Память не выделилась корректно\n");
 		exit(2);
 	}
 
@@ -52,36 +51,12 @@ void pushback(dblLinkedList** list, void* value, size_t value_size) {
 	if ((*list)->head == NULL) {
 		(*list)->head = tmp;
 	}
-	(*list)->size++;
 }
 
-Node* getNode(dblLinkedList* list, size_t index) {
-	if (index > list->size) {
-		printf("Индекс больше возможного");
-		exit(4);
-	}
-	Node* tmp = NULL;
-	if (index < list->size / 2) {
-		tmp = list->head;
-		for (size_t i = 0; i < index && tmp; i++) {
-			tmp = tmp->next;
-		}
-	}
-	else {
-		tmp = list->tail;
-		for (size_t i = list->size - 1; i > index && tmp; i--) {
-			tmp = tmp->prev;
-		}
-	}
-
-	return tmp;
-}
-
-void popBack(dblLinkedList* list) {
+void* popBack(dblLinkedList* list) {
 	Node* tmp;
-
 	if (list->tail == NULL) {
-		printf("Удалять нечего для popBack");
+		perror("Удалять нечего для popBack");
 		exit(3);
 	}
 	tmp = list->tail;
@@ -92,20 +67,15 @@ void popBack(dblLinkedList* list) {
 	if (tmp == list->head) {
 		list->head = NULL;
 	}
-	__sync_fetch_and_sub(&list->size, 1);
-	free(tmp->value);
+	void* value = tmp->value;
 	free(tmp);
-	if (list->size == 0) {
-		list->head = NULL;
-		list->tail = NULL;
-	}
+	return value;
 }
 
-void popFront(dblLinkedList* list) {
+void* popFront(dblLinkedList* list) {
 	Node* tmp;
-
 	if (list->head == NULL) {
-		printf("Удалять нечего для popBack");
+		perror("Удалять нечего для popFront");
 		exit(3);
 	}
 	tmp = list->head;
@@ -116,36 +86,9 @@ void popFront(dblLinkedList* list) {
 	if (tmp == list->tail) {
 		list->tail = NULL;
 	}
-	__sync_fetch_and_sub(&list->size, 1);
-	free(tmp->value);
+	void* value = tmp->value;
 	free(tmp);
-	if (list->size == 0) {
-		list->head = NULL;
-		list->tail = NULL;
-	}
-}
-
-void delNode(dblLinkedList* list, size_t index) {
-	Node* elm = getNode(list, index);
-	if (elm == NULL) {
-		printf("Удалять нечего для popBack");
-		exit(3);
-	}
-	if (elm->prev) {
-		elm->prev->next = elm->next;
-	}
-	if (elm->next) {
-		elm->next->prev = elm->prev;
-	}
-	if (!elm->prev) {
-		list->head = elm->next;
-	}
-	if (!elm->next) {
-		list->tail = elm->prev;
-	}
-	__sync_fetch_and_sub(&list->size, 1);
-	free(elm->value);
-	free(elm);
+	return value;
 }
 
 //	Чтобы работало для разных типов данных, надо описать функцию печати для этих типов
